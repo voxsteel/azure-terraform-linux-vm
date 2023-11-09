07c00dde2f9d4edc39333ccbb6bbad4bc00d929f
Viorel Rudareanu <24523236+voxsteel@users.noreply.github.com>
2023-11-09 16:02:37 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,49 +2,66 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = var.location
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+resource "azurerm_subnet" "internal" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    name                          = "${var.prefix}-ip-config"
+    subnet_id                     = azurerm_subnet.internal.id
+    public_ip_address_id          = azurerm_public_ip.main.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "example" {
-  name                = "example-machine"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_linux_virtual_machine" "main" {
+  name                = "${var.prefix}-vm"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
   size                = "Standard_F2"
-  admin_username      = "adminuser"
-  network_interface_id = azurerm_network_interface.example.id
+  network_interface_id = azurerm_network_interface.main.id
 
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
+
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = var.linux_admin_username
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+# Update the path to your own public key  
+    ssh_keys {
+      path     = "/home/${var.linux_admin_username}/.ssh/authorized_keys"
+      key_data = file("~/.ssh/unused/id_rsa.pub")
+      #      key_data = var.ssh_public_key
+    }
+ } 
+
 
   source_image_reference {
     publisher = "Canonical"
@@ -54,11 +71,4 @@ resource "azurerm_linux_virtual_machine" "example" {
   }
 }
 
-# Update the path to your own public key  
-    ssh_keys {
-      path     = "/home/${var.linux_admin_username}/.ssh/authorized_keys"
-      key_data = file("~/.ssh/unused/id_rsa.pub")
-      #      key_data = var.ssh_public_key
-    }
-  }
-}
+
