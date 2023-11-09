162bda7844cdb449073d6fc32bb60c95a6801404
Viorel Rudareanu <24523236+voxsteel@users.noreply.github.com>
2023-11-09 16:50:04 +0000

    added main

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
 provider "azurerm" {
   features {}
 }
@@ -9,13 +12,13 @@ resource "azurerm_resource_group" "main" {
 
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
   address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 }
 
 resource "azurerm_subnet" "internal" {
-  name                 = "${var.prefix}-subnet"
+  name                 = "internal"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
@@ -23,52 +26,41 @@ resource "azurerm_subnet" "internal" {
 
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
 
   ip_configuration {
-    name                          = "${var.prefix}-ip-config"
+    name                          = "internal"
     subnet_id                     = azurerm_subnet.internal.id
-    public_ip_address_id          = azurerm_public_ip.main.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
-  name                = "${var.prefix}-vm"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  size                = "Standard_F2"
-  network_interface_id = azurerm_network_interface.main.id
+  name                            = "${var.prefix}-vm"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  size                            = "Standard_F2"
+  admin_username                  = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+  admin_ssh_key {
+    username = "voxsteel"
+    #path     = "/home/voxsteel/.ssh/authorized_keys"
+    public_key = file("~/.ssh/unused/id_rsa.pub")
   }
-
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = var.linux_admin_username
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = true
-
-# Update the path to your own public key  
-    ssh_keys {
-      path     = "/home/${var.linux_admin_username}/.ssh/authorized_keys"
-      key_data = file("~/.ssh/unused/id_rsa.pub")
-      #      key_data = var.ssh_public_key
-    }
- } 
-
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
     version   = "latest"
   }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
 }
-
-
