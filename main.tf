b0031cdf4dfefc516951b6e6548656d9e792986c
Viorel Rudareanu <24523236+voxsteel@users.noreply.github.com>
2023-11-16 16:55:23 +0000

    changed main

--- main.tf
+++ main.tf
@@ -1,17 +1,16 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 provider "azurerm" {
   features {}
   skip_provider_registration = true
   subscription_id            = "683c85cb-860b-4cc1-bca8-ef6df1ee662d"
 }
 
+# Create a resource group
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
   location = var.location
 }
 
+# Create a network
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
@@ -19,50 +18,95 @@ resource "azurerm_virtual_network" "main" {
   resource_group_name = azurerm_resource_group.main.name
 }
 
+# Create a subnet
 resource "azurerm_subnet" "internal" {
-  name                 = "internal"
+  name                 = "${var.prefix}-subnet"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes       = ["10.0.1.0/24"]
 }
 
+# Create public IP
+resource "azurerm_public_ip" "main" {
+  name                = "${var.prefix}-public-ip"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+}
+
+# Create network Interface
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
-  resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
   ip_configuration {
-    name                          = "internal"
+    name                          = "${var.prefix}-ip-config"
     subnet_id                     = azurerm_subnet.internal.id
+    public_ip_address_id          = azurerm_public_ip.main.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "main" {
-  name                            = "${var.prefix}-vm"
-  resource_group_name             = azurerm_resource_group.main.name
-  location                        = azurerm_resource_group.main.location
-  size                            = "Standard_F2"
-  admin_username                  = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.main.id,
-  ]
+# Create security group and rules to open needed ports
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.prefix}-security-group"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
 
-  admin_ssh_key {
-    username = "adminuser"
-    #path     = "/home/voxsteel/.ssh/authorized_keys"
-    public_key = file("~/.ssh/unused/id_rsa.pub")
-  }
+resource "azurerm_network_security_rule" "main" {
+  name                        = "${var.prefix}-security-rules"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  destination_port_ranges     = ["22", "25", "80", "122", "443", "8080", "8443", "9418"]
+  source_port_range           = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.main.name
+}
 
-  source_image_reference {
+# Create Virtual machine 
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_D2s_v3"
+
+  storage_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
   }
 
-  os_disk {
-    storage_account_type = "Standard_LRS"
-    caching              = "ReadWrite"
+# Create OS Disk
+  storage_os_disk {
+    name              = "${var.prefix}-os-storage"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+    disk_size_gb      = "200"
+  }
+
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = var.linux_admin_username
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+# Update the path to your own public key  
+    ssh_keys {
+      path     = "/home/${var.linux_admin_username}/.ssh/authorized_keys"
+      key_data = file("~/.ssh/unused/id_rsa.pub")
+      #      key_data = var.ssh_public_key
+    }
   }
 }
+
